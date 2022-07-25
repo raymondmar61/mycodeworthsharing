0035f5ede8a1a6b0a4989fcbd3f64ba5f383ce79
raymondmar61 <raym61@hotmail.com>
2022-07-25 15:23:34 -0700

    Added sql join function to view artists and his or her arts, clear artist table, clear pieces table, and export to text file completed

--- artgallery150.py
+++ artgallery150.py
@@ -1,56 +1,40 @@
 import sqlite3
+import os
 
-def addartist(artistid, name, address, town, country, postcode):
-    # def addartist():
-    # db = sqlite3.connect("artgallery.db")
-    # cursor = db.cursor()
-    # cursor.execute("SELECT count(*) from artists;")
-    # artistid = cursor.fetchall()[0][0] + 1
-    # name = input("Enter artist's name: ")
-    # address = input("Enter artist's address: ")
-    # town = input("Enter artist's town: ")
-    # country = input("Enter artist's country: ")
-    # postcode = input("Enter artist's postcode: ")
-    cursor.execute("""
-    INSERT into artists values(?,?,?,?,?,?);
-    """, (artistid, name, address, town, country, postcode))
-    # db.commit()
-    # db.close()
+def addartist():
+    cursor.execute("SELECT count(*) from artists;")
+    #Automatically create an artist id
+    artistid = cursor.fetchall()[0][0] + 1
+    name = input("Enter artist's name: ")
+    address = input("Enter artist's address: ")
+    town = input("Enter artist's town: ")
+    country = input("Enter artist's country: ")
+    postcode = input("Enter artist's postcode: ")
+    cursor.execute("INSERT into artists values(?,?,?,?,?,?);", (artistid, name, address, town, country, postcode))
+    db.commit()
 
-def addpiece(pieceid, artistid, title, medium, price):
-    # def addpiece():
-    # db = sqlite3.connect("artgallery.db")
-    # cursor = db.cursor()
-    # cursor.execute("SELECT count(*) from pieces;")
-    # pieceid = cursor.fetchall()[0][0] + 1
-    # artistid = int(input("Enter artist's id: "))
-    # title = input("Enter piece's title: ")
-    # medium = input("Enter piece's medium: ")
-    # price = int(input("Enter piece's price: "))
-    cursor.execute("""
-    INSERT into pieces values(?,?,?,?,?);
-    """, (pieceid, artistid, title, medium, price))
-    # db.commit()
-    # db.close()
+def addpiece():
+    cursor.execute("SELECT count(*) from pieces;")
+    #Automatically create an art piece id
+    pieceid = cursor.fetchall()[0][0] + 1
+    artistid = int(input("Enter artist's id: "))
+    title = input("Enter piece's title: ")
+    medium = input("Enter piece's medium: ")
+    price = int(input("Enter piece's price: "))
+    cursor.execute("INSERT into pieces values(?,?,?,?,?);", (pieceid, artistid, title, medium, price))
+    db.commit()
 
 def soldpiece():
-    # def soldpiece(pieceid):
-    # db = sqlite3.connect("artgallery.db")
-    # cursor = db.cursor()
-    # pieceid = input("Enter sold piece id:")
-    pieceid = "2"
-    cursor.execute("SELECT * from pieces where pieceid = (?)", (pieceid))
+    pieceid = input("Enter sold piece id: ")
+    cursor.execute("SELECT * from pieces where pieceid = (?);", (pieceid,))
     soldpiece = str(cursor.fetchall()[0])
     print("Sold piece:", soldpiece)
     #Write the sold art piece to a text file
     with open("soldpiecetextfile.txt", "a") as textfileobject:
         textfileobject.write(soldpiece)
         textfileobject.write("\n")
-    cursor.execute("""
-    DELETE from pieces
-    where pieceid = (?);""", (pieceid))
-    # db.commit()
-    # db.close()
+    cursor.execute("DELETE from pieces where pieceid = (?);", (pieceid,))
+    db.commit()
 
 def viewartist():
     cursor.execute("SELECT * from artists;")
@@ -60,29 +44,72 @@ def viewpieceofart():
     cursor.execute("SELECT * from pieces;")
     print(cursor.fetchall())
 
+#Write a SQL join to view the artist and his or her pieces of art
+def viewartistpieceofart():
+    cursor.execute("""
+        SELECT a.name, p.title, p.medium, p.price
+        from artists a join pieces p
+        on a.artistid = p.artistid;""")
+    print(cursor.fetchall())
+
 def searchartist():
     searchartistname = input("Enter artist name: ")
     searchartistname = "%" + searchartistname + "%"
-    cursor.execute("""SELECT * from artists where name like (?);""", (searchartistname,))
+    cursor.execute("SELECT * from artists where name like (?);", (searchartistname,))
     print(cursor.fetchall())
 
 def searchpieceofartname():
     searchpieceofartname = input("Enter piece of art name: ")
     searchpieceofartname = "%" + searchpieceofartname + "%"
-    cursor.execute("""SELECT * from pieces where title like (?);""", (searchpieceofartname,))
+    cursor.execute("SELECT * from pieces where title like (?);", (searchpieceofartname,))
     print(cursor.fetchall())
 
 def searchpieceofartmedium():
     searchpieceofartmedium = input("Enter piece of art medium: ")
     searchpieceofartmedium = "%" + searchpieceofartmedium + "%"
-    cursor.execute("""SELECT * from pieces where medium like (?);""", (searchpieceofartmedium,))
+    cursor.execute("SELECT * from pieces where medium like (?);", (searchpieceofartmedium,))
     print(cursor.fetchall())
 
 def searchpieceofartprice():
     priceinput = input("Enter piece of art price range separated by a space.  For example, 100 200: ")
     splitprice = priceinput.split()
-    cursor.execute("""SELECT * from pieces where price between (?) and (?);""", (splitprice[0], splitprice[1]))
+    cursor.execute("SELECT * from pieces where price between (?) and (?);", (splitprice[0], splitprice[1]))
     print(cursor.fetchall())
+
+def exportartist():
+    #remove existing artiststextfile.txt to create a new artiststextfile.txt file.  Try except in case artiststextfile.txt file doesn't exist
+    try:
+        os.remove("artiststextfile.txt")
+    except FileNotFoundError:
+        filewrite = open("artiststextfile.txt", "w")
+        filewrite.close()
+    cursor.execute("SELECT * from artists;")
+    artiststotextfile = cursor.fetchall()
+    for eachartiststotextfile in artiststotextfile:
+        with open("artiststextfile.txt", "a") as textfileobject:
+            textfileobject.write(str(eachartiststotextfile))
+            textfileobject.write("\n")
+
+def exportpieceofartprice():
+    #remove existing piecesofarttextfile.txt to create a new piecesofarttextfile.txt file.  Try except in case piecesofarttextfile.txt file doesn't exist
+    try:
+        os.remove("piecesofarttextfile.txt")
+    except FileNotFoundError:
+        filewrite = open("piecesofarttextfile.txt", "w")
+        filewrite.close()
+    cursor.execute("SELECT * from pieces;")
+    piecesofarttotextfile = cursor.fetchall()
+    for eachpiecesofarttotextfile in piecesofarttotextfile:
+        with open("piecesofarttextfile.txt", "a") as textfileobject:
+            textfileobject.write(str(eachpiecesofarttotextfile))
+            textfileobject.write("\n")
+
+def clearartisttable():
+    cursor.execute("DELETE from artists;")
+    db.commit()
+def clearpiecettable():
+    cursor.execute("DELETE from pieces;")
+    db.commit()
 
 
 #Connect to artgallery database
@@ -103,21 +130,23 @@ Please choose one of these options:
 3) Enter sold piece of art
 4) View artists
 5) View pieces of art
-6) Search artist
-7) Search piece of art name
-8) Search piece of art medium
-9) Search piece of art price
-10) Exit
-11) Temp test add new artist and new piece of art
+6) View artists and their pieces of art
+7) Search artist
+8) Search piece of art name
+9) Search piece of art medium
+10) Search piece of art price
+11) Export artists to text file
+12) Export pieces of art to text file
+13) Exit
+14) Clear artist table
+15) Clear pieces table
 
 Your selection: """
-while (userinput := input(menuprompt)) != "10":  #RM:  need paranthesis for (userinput := input(menuprompt))
+while (userinput := input(menuprompt)) != "13":  #RM:  need paranthesis for (userinput := input(menuprompt))
     if userinput == "1":
-        addartist(1, "Martin Leighton", "5 Park Place", "Peterborough", "Cambridgeshire", "PE32 5LP")
-        addartist(2, "Eva Czarniecka", "77 Warner Close", "Chelmsford", "Essex", "CM22 5FT")
+        addartist()
     elif userinput == "2":
-        addpiece(4, 5, "Woman with black Labrador", "Oil", 220)
-        addpiece(2, 5, "Bees & thistles", "Watercolour", 85)
+        addpiece()
     elif userinput == "3":
         soldpiece()
     elif userinput == "4":
@@ -125,42 +154,26 @@ while (userinput := input(menuprompt)) != "10":  #RM:  need paranthesis for (use
     elif userinput == "5":
         viewpieceofart()
     elif userinput == "6":
-        searchartist()
+        viewartistpieceofart()
     elif userinput == "7":
-        searchpieceofartname()
+        searchartist()
     elif userinput == "8":
-        searchpieceofartmedium()
+        searchpieceofartname()
     elif userinput == "9":
-        searchpieceofartprice()
+        searchpieceofartmedium()
     elif userinput == "10":
-        break
+        searchpieceofartprice()
     elif userinput == "11":
-        addartist(1, "Martin Leighton", "5 Park Place", "Peterborough", "Cambridgeshire", "PE32 5LP")
-        addartist(2, "Eva Czarniecka", "77 Warner Close", "Chelmsford", "Essex", "CM22 5FT")
-        addpiece(4, 5, "Woman with black Labrador", "Oil", 220)
-        addpiece(2, 5, "Bees & thistles", "Watercolour", 85)
+        exportartist()
+    elif userinput == "12":
+        exportpieceofartprice()
+    elif userinput == "13":
+        break
+    elif userinput == "14":
+        clearartisttable()
+    elif userinput == "15":
+        clearpiecettable()
     else:
         print("Invalid input.  Please try again.")
-cursor.execute("SELECT * from artists;")
-print(cursor.fetchall())
-cursor.execute("SELECT * from pieces;")
-print(cursor.fetchall())
-cursor.execute("""
-DROP table artists;
-""")
-cursor.execute("""
-DROP table pieces;
-""")
-db.commit()
+    print("\n")
 db.close()
-
-#Reminder pass a function value to another function
-def secondfunction(a):
-    print(a + 1)
-
-def firstfunction(n):
-    secondfunction(n + 1)
-    return n
-
-
-print(firstfunction(5))
